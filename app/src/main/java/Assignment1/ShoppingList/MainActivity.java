74e55c8dba6bd0d29eb7986c09b4d34158ed6a01
JariKin <jari.kinnunen@edu.savonia.fi>
2019-09-12 20:53:17 +0300

    commit message

---
+++ app/src/main/java/Assignment1/ShoppingList/MainActivity.java
@@ -0,0 +1,46 @@
+package Assignment1.ShoppingList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText item;
+    TextView textView;
+    Button save;
+    Button done;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        item = findViewById(R.id.id_editText_1);
+        save = findViewById(R.id.id_button_1);
+        done = findViewById(R.id.id_button_2);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(item.length() < 3 || item.length() > 15) {
+                    Toast.makeText(MainActivity.this, R.string.WRONG, Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(MainActivity.this, R.string.RIGHT, Toast.LENGTH_LONG).show();
+                }
+            }
+            });
+
+
+
+    }
+
+}
---
+++ app/src/main/java/Assignment1/ShoppingList/ShowListActivity.java
@@ -0,0 +1,14 @@
+package Assignment1.ShoppingList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_list2);
+    }
+}
